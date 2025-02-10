ad43ad6484ac8de92fcbd6c899781c2d2ef61e64
proboynick <kuzmi495@mail.ru>
2025-02-10 21:08:03 +0300

    feat: addd select input for future filter

--- LabinventCharts/src/app/Components/tools/tools.component.ts
+++ LabinventCharts/src/app/Components/tools/tools.component.ts
@@ -2,16 +2,50 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CheckboxChangeEvent, CheckboxModule } from 'primeng/checkbox';
 import { setIsRemoveZeroValues } from '../../Redux/files-store.actions';
+import { SelectChangeEvent, SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-tools',
-  imports: [CheckboxModule],
+  imports: [CheckboxModule, SelectModule],
   templateUrl: './tools.component.html',
   styleUrl: './tools.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToolsComponent {
   constructor(private store: Store) {}
+
+  alphabet: string[] = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+
+  setFilterValue(event: SelectChangeEvent) {
+    console.log(event.value);
+  }
 
   toggleZeroValues(event: CheckboxChangeEvent) {
     const isChecked = event.checked;
